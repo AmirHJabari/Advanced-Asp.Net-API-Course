2515c7ccbad38b73aabdab79e71e018881e5fc03
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-10-14 18:43:50 +0330

    Add DbContext.

---
+++ src/AdvancedAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Common.Utilities;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var entitiesAssembly = typeof(IEntity).Assembly;
+
+
+            // modelBuilder.RegisterEntityTypeConfiguration(entitiesAssembly); // my old method
+            modelBuilder.ApplyConfigurationsFromAssembly(entitiesAssembly);
+            modelBuilder.RegisterAllEntities<IEntity>(entitiesAssembly);
+            modelBuilder.AddRestrictDeleteBehaviorConvention();
+            modelBuilder.AddSequentialGuidForIdConvention();
+            modelBuilder.AddPluralizingTableNameConvention();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void CleanString()
+        {
+            var changedEntities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+            foreach (var item in changedEntities)
+            {
+                if (item.Entity == null)
+                    continue;
+
+                var properties = item.Entity.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(p => p.CanRead && p.CanWrite && p.PropertyType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    var propName = property.Name;
+                    var val = (string)property.GetValue(item.Entity, null);
+
+                    if (val.HasValue())
+                    {
+                        var newVal = val.Fa2En().FixPersianChars();
+                        if (newVal == val)
+                            continue;
+                        property.SetValue(item.Entity, newVal, null);
+                    }
+                }
+            }
+        }
+    }
+}
