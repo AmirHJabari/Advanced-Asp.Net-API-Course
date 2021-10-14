8a34169922678425beae5c391348e2710e60ce16
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-10-14 17:05:49 +0330

    Add EF Entities.

---
+++ src/AdvancedAPI/Entities/Post/Post.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Post : BaseEntity<Guid>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
+    }
+
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // Title
+            builder.Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            // Description
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(2500);
+
+            // Author
+            builder.HasOne(p => p.Author)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.AuthorId);
+
+            // Category
+            builder.HasOne(p => p.Category)
+                .WithMany(c => c.Posts)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
---
+++ src/AdvancedAPI/Entities/User/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User : BaseEntity<int>
+    {
+        public User()
+        {
+            this.IsActive = true;
+        }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Range(1, 150)]
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset LastLogin { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+
+    public enum Gender
+    {
+        [Display(Name = "هیچکدام")]
+        None,
+
+        [Display(Name = "زن")]
+        Female,
+
+        [Display(Name = "مرد")]
+        Male,
+    }
+}
