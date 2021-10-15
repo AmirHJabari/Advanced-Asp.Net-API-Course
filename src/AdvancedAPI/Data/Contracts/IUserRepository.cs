b4f4fe66eee76314acbc056cdb66d693d996ea81
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-10-15 11:10:09 +0330

    Add Database Repositories.

---
+++ src/AdvancedAPI/Data/Contracts/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetUserByUserNameAndPasswordAsync(string userName, string password, CancellationToken cancellationToken);
+        Task AddAsync(User user, string password, CancellationToken cancellationToken, bool saveNow = true);
+        Task<bool> IsUserNameExistAsync(string userName, CancellationToken cancellationToken);
+        Task<bool> IsUserExistAsync(int id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/AdvancedAPI/Data/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Utilities;
+
+namespace Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+        { }
+
+        public Task AddAsync(User user, string password, CancellationToken cancellationToken, bool saveNow = true)
+        {
+            user.PasswordHash = SecurityHelper.GetSha256Hash(password);
+            return base.AddAsync(user, cancellationToken, saveNow);
+        }
+
+        public Task<bool> IsUserNameExistAsync(string userName, CancellationToken cancellationToken)
+        {
+            return this.TableNoTracking.AnyAsync(u => u.UserName == userName, cancellationToken);
+        }
+
+        public Task<User> GetUserByUserNameAndPasswordAsync(string userName, string password, CancellationToken cancellationToken)
+        {
+            string passwordHash = SecurityHelper.GetSha256Hash(password);
+
+            return Entities.FirstOrDefaultAsync(u =>
+                        u.UserName == userName && u.PasswordHash == passwordHash,
+                        cancellationToken);
+        }
+
+        public Task<bool> IsUserExistAsync(int id, CancellationToken cancellationToken)
+        {
+            return this.TableNoTracking.AnyAsync(u => u.Id == id, cancellationToken);
+        }
+    }
+}
