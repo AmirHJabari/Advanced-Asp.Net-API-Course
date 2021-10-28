893b357359aca372df080720f6f66b135304029b
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-10-28 16:29:37 +0330

    Add ApiResultFilter attribute in WebFramework.

---
+++ src/AdvancedAPI/WebFramework/Filters/ApiResultFilterAttribute.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFramework.API;
+
+namespace WebFramework.Filters
+{
+    /*
+        OnActionExecutionAsync
+        OnActionExecuting
+- action is executing
+        OnActionExecuted
+        OnResultExecutionAsync
+        OnResultExecuting
+        OnResultExecuted
+     */
+
+    /// <summary>
+    /// Just for test and sample.
+    /// </summary>
+    public class ApiResultFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("OnActionExecuted");
+            base.OnActionExecuted(context);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("OnActionExecuting");
+            base.OnActionExecuting(context);
+        }
+
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Console.WriteLine("OnActionExecutionAsync");
+            return base.OnActionExecutionAsync(context, next);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            Console.WriteLine("OnResultExecuted");
+            base.OnResultExecuted(context);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            Console.WriteLine("OnResultExecuting");
+
+            if (context.Result is OkObjectResult okObjectResult)
+            {
+                var apiResult = new ApiResult<object>().WithData(okObjectResult.Value);
+                context.Result = new JsonResult(apiResult) { StatusCode = okObjectResult.StatusCode };
+            }
+            else if (context.Result is OkResult okResult)
+            {
+                var apiResult = new ApiResult();
+                context.Result = new JsonResult(apiResult) { StatusCode = okResult.StatusCode };
+            }
+            else if (context.Result is BadRequestResult badRequestResult)
+            {
+                var apiResult = new ApiResult(false);
+                context.Result = new JsonResult(apiResult) { StatusCode = badRequestResult.StatusCode };
+            }
+            else if (context.Result is BadRequestObjectResult badRequestObjectResult)
+            {
+                var message = badRequestObjectResult.Value.ToString();
+                if (badRequestObjectResult.Value is SerializableError errors)
+                {
+                    var errorMessages = errors.SelectMany(p => (string[])p.Value).Distinct();
+                    message = string.Join(" | ", errorMessages);
+                }
+                var apiResult = new ApiResult(false, message);
+                context.Result = new JsonResult(apiResult) { StatusCode = badRequestObjectResult.StatusCode };
+            }
+            else if (context.Result is ContentResult contentResult)
+            {
+                var apiResult = new ApiResult().WithMessage(contentResult.Content);
+                context.Result = new JsonResult(apiResult) { StatusCode = contentResult.StatusCode };
+            }
+            else if (context.Result is NotFoundResult notFoundResult)
+            {
+                var apiResult = new ApiResult(false);
+                context.Result = new JsonResult(apiResult) { StatusCode = notFoundResult.StatusCode };
+            }
+            else if (context.Result is NotFoundObjectResult notFoundObjectResult)
+            {
+                var apiResult = new ApiResult<object>(false).WithData(notFoundObjectResult.Value);
+                context.Result = new JsonResult(apiResult) { StatusCode = notFoundObjectResult.StatusCode };
+            }
+            else if (context.Result is ObjectResult objectResult && objectResult.StatusCode == null
+                && !(objectResult.Value is ApiResult))
+            {
+                var apiResult = new ApiResult<object>().WithData(objectResult.Value);
+                context.Result = new JsonResult(apiResult) { StatusCode = objectResult.StatusCode };
+            }
+
+            base.OnResultExecuting(context);
+        }
+
+        public override Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            Console.WriteLine("OnResultExecutionAsync");
+            return base.OnResultExecutionAsync(context, next);
+        }
+    }
+}
