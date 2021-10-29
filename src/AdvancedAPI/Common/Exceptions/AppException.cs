185daa0d53d3c7eb7af5bf2da67d97f3bd540c00
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-10-29 23:28:02 +0330

    Add Custom Exceptions.

---
+++ src/AdvancedAPI/Common/Exceptions/AppException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Exceptions
+{
+    public class AppException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public int Code { get; set; }
+        public object AdditionalData { get; set; }
+        
+        public AppException(string message,
+            int code = -1,
+            HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError,
+            object additionalData = null,
+            Exception exception = null)
+            : base(message, exception)
+        {
+            Code = code;
+            HttpStatusCode = httpStatusCode;
+            AdditionalData = additionalData;
+        }
+    }
+}
---
+++ src/AdvancedAPI/Common/Exceptions/LogicException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Exceptions
+{
+    public class LogicException : AppException
+    {
+        public LogicException(string message, int code = -1, HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError, object additionalData = null, Exception exception = null) 
+            : base(message, code, httpStatusCode, additionalData, exception)
+        {
+        }
+    }
+}
