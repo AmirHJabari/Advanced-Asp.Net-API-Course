0f50cae7cdfdfb007321631c183a2f866cf77de8
Amir H. Jabari <amirhamzehjabari@gmail.com>
2021-11-05 13:02:12 +0330

    Add ServiceCollectionExtensions and AddJwtAuthentication method.

---
+++ src/AdvancedAPI/WebFramework/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Common;
+
+namespace WebFramework.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, JwtSettings settings)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    var secretKey = Encoding.UTF8.GetBytes(settings.SecretKey);
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ClockSkew = TimeSpan.FromMinutes(settings.ClockSkewMinutes), // default: 5m
+                        RequireSignedTokens = true,
+
+                        IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                        ValidateIssuerSigningKey = true,
+
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true,
+
+                        ValidateAudience = true, // default: false
+                        ValidAudience = settings.Audience,
+
+                        ValidateIssuer = true, // default: false
+                        ValidIssuer = settings.Issuer
+                    };
+
+                    options.RequireHttpsMetadata = true;
+                    options.SaveToken = true;
+                });
+
+            return services;
+        }
+    }
+}
